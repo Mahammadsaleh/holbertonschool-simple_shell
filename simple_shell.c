7d1f6404ee85399097ef44774d06b64c0e37a3f3
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-29 09:13:43 -0800

    potentional fixes

--- simple_shell.c
+++ simple_shell.c
@@ -6,20 +6,19 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #define CHAR_BUFFER 1024
-extern char **environ;
 /**
  * line_devider - devide the line
  * @buffer: string
  *
  * Return: char ptr to ptr
  */
-char **line_divider(char *buffer, char **arr)
+char **line_devider(char *buffer, char **arr)
 {
-		char *token;
+	char *token;
 	int i = 0;
 
 	token = strtok(buffer, " \n\t");
-	while (token && i < 63)
+	while (token != NULL && i < 63)
 	{
 		arr[i++] = token;
 		token = strtok(NULL, " \n\t");
@@ -36,19 +35,38 @@ char **line_divider(char *buffer, char **arr)
  */
 char *get_input(void)
 {
-	        char *buffer = NULL;
+	char *buffer = NULL;
         size_t len = 0;
-        int read;
-
-        read = getline(&buffer, &len, stdin);
-        if (read == -1)
-        {
-                free(buffer);
-                return (NULL);
-        }
-        if (buffer[read - 1] == '\n')
+	int read;
+	if (isatty(STDIN_FILENO))
+		printf("$ ");
+	read = getline(&buffer, &len, stdin);
+	if (read == -1)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	if (buffer[read - 1] == '\n')
                 buffer[read - 1] = '\0';
-        return (buffer);
+	return (buffer);
+}
+/**
+ * free_array - free array memory
+ * @arr: buffer
+ *
+ * Return - nothing
+ */
+void free_array(char **arr)
+{
+	int i;
+	if (arr == NULL)
+		return;
+	for (i = 0; arr[i] != NULL; i++)
+	{
+		free(arr[i]);
+	}
+	free(arr);
+	arr = NULL;
 }
 /**
  * path_handler - path handler
@@ -58,108 +76,42 @@ char *get_input(void)
  *
  * return: file_name
  */
-int execute(char *buffer)
+char *path_handler(char *file_name, char *path)
 {
-	int status = 0;
-	pid_t pid = fork();
-
-	if (pid == -1)
+	char *token = strtok(path, ":");
+	char cmd[100];
+	if (file_name[0] == '/')
 	{
-		perror("ERROR");
-		free(buffer);
-		exit(EXIT_FAILURE);
+		if (access(file_name, X_OK) == 0)
+		{
+			return strdup(file_name);
+		}
 	}
-	else if (pid == 0)
+	while (token)
 	{
-		char *arr[100];
-		line_divider(buffer, arr);
-		if (arr[0] == NULL)
+		snprintf(cmd, sizeof(cmd), "%s/%s", token, file_name);
+		if (access(cmd, X_OK) == 0)
 		{
-			free(buffer);
-			exit(EXIT_SUCCESS);
+			return strdup(cmd);
 		}
-		if (strcmp(arr[0], "env") == 0)
-		{
-			char **env = environ;
-
-			while (*env != NULL)
-			{
-				printf("%s\n", *env);
-				env++;
-			}
-			free(buffer);
-			exit(EXIT_SUCCESS);
-		}
-		if (strchr(arr[0], '/') != NULL)
-		{
-			if (access(arr[0], X_OK) == 0)
-			{
-				if (execve(arr[0], arr, environ) == -1)
-				{
-					perror("execve");
-					free(buffer);
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-		else
-		{
-			char *path = getenv("PATH");
-			char *token;
-			if (path == NULL)
-			{
-				char error_message[CHAR_BUFFER];
-				snprintf(error_message, sizeof(error_message), "./hsh: 1: %s: not found\n", arr[0]);
-				write(STDERR_FILENO, error_message, strlen(error_message));
-				free(buffer);
-				exit(127);
-			}
-			token = strtok(path, ":");
-			while (token)
-			{
-				char cmd[100];
-				snprintf(cmd, sizeof(cmd), "%s/%s", token, arr[0]);
-				if (access(cmd, X_OK) == 0)
-				{
-					if (execve(cmd, arr, environ) == -1)
-					{
-						perror("execve");
-						free(buffer);
-						exit(EXIT_FAILURE);
-					}
-				}
-				token = strtok(NULL, ":");
-			}
-		}
-		fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
-		free(buffer);
-		exit(127);
+		token = strtok(NULL, ":");
 	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		free(buffer);
-		if (WIFEXITED(status))
-			status = WEXITSTATUS(status);
-		else
-			status = 1;
-	}
-	return (status);
+	return strdup(file_name);
 }
 /**
  * main - main func
  *
  * Return: int
  */
-int main(void)
+int main(int argc, char **argv)
 {
-	char *buffer;
-	int status;
-	
+	extern char **environ;
+	char *buffer = NULL;
+	size_t len = argc * 512;
+	int status = 0;
+	pid_t pid;
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			printf("$ ");
 		buffer = get_input();
 		if (buffer == NULL)
 			break;
@@ -168,11 +120,63 @@ int main(void)
 			free(buffer);
 			exit(0);
 		}
-		status = execute(buffer);
-		if (status == 2 && isatty(STDIN_FILENO))
+		pid = fork();
+		if (pid == 0)
 		{
-			exit(2);
+			char *arr[100];
+			line_devider(buffer, arr);
+			char *path = getenv("PATH");
+			char *original_command = strdup(arr[0]);
+			if (path == NULL || *path == '\0')
+			{
+				if (arr[0][0] == '/')
+				{
+					arr[0] = path_handler(arr[0], path);
+					if (execve(arr[0], arr, environ) == -1)
+					{
+						perror("ERROR");
+						free_array(arr);
+						free(buffer);
+						exit(1);
+					}
+				}
+				else
+				{
+					char error_message[CHAR_BUFFER];
+					snprintf(error_message, sizeof(error_message), "%s: 1: %s: not found\n", argv[0], original_command);
+					write(STDERR_FILENO, error_message, strlen(error_message));
+					exit(127);
+				}
+			}
+			arr[0] = path_handler(arr[0], path);
+			if (execve(arr[0], arr, environ) == -1)
+			{
+				perror("ERROR");
+				exit(1);
+			}
 		}
+		else if (pid > 0)
+		{
+			if (waitpid(pid, &status, 0) == -1)
+			{
+				perror("ERROR");
+			}
+			if (WIFEXITED(status))
+			{
+				int exit_status = WEXITSTATUS(status);
+				if (exit_status == 127)
+				{
+					exit(127);
+				}
+			}
+		}
+		else
+		{
+			perror("ERROR");
+		}
+		if (pid == -1)
+			perror("ERROR");
 	}
+	free(buffer);
 	return (status);
 }
