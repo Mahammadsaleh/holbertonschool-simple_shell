5797adcf8de22fe2c7dd3fad66005da4f200ba7f
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-30 02:57:11 -0800

    main.h created

--- main.h
+++ main.h
@@ -6,9 +6,7 @@
 #include <unistd.h>
 extern char **environ;
 
-int delim_counter(char *str, char *delim);
-char **token_parse(char *str, char *delim);
-char *check_file(char **path_arr, char *filepath, int *flag);
-int execute(char **args, char *filename, int *status);
-void _printenv(void);
+char **line_devider(char *buffer, char **arr);
+char *get_input(void);
+int execute(char *buffer);
 #endif
