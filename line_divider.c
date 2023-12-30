1c9895e2b9aea6eef0897d776e47137d9fa95033
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-30 03:01:44 -0800

    line_divider created

---
+++ line_divider.c
@@ -0,0 +1,20 @@
+ /**
+ * line_devider - devide the line
+ * @buffer: string
+ *
+ * Return: char ptr to ptr
+ */
+ char **line_devider(char *buffer, char **arr)
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(buffer, " \n\t");
+	while (token != NULL && i < 63)
+	{
+		arr[i++] = token;
+		token = strtok(NULL, " \n\t");
+	}
+	arr[i++] = NULL;
+	return (arr);
+}
